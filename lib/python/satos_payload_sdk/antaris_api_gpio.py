676d6a5b45035a714f9b6b585e9fc92e37c3b1b8
Brian Waldon <brian@antaris.space>
2023-05-23 13:08:05 -0700

    Remove defunct python payload app
    This has been replaced by an example app in examples/app-python.
    
    The GPIO example now lives in the antaris_api_gpio module.

--- lib/python/satos_payload_sdk/antaris_api_gpio.py
+++ lib/python/satos_payload_sdk/antaris_api_gpio.py
@@ -76,3 +76,26 @@ def api_pa_pc_write_gpio(pin, value):
     time.sleep(g_SLEEP_TIME_IN_SEC)
     op = (Device.port >> pin) & g_MASK_BIT_0
     return op
+
+
+if __name__ == "__main__":
+    #sample test program
+    val = api_gpio. api_pa_pc_read_gpio(int(sys.argv[1]))
+    if val != g_GPIO_ERROR:
+        print("initial Gpio vaue of  ", int(sys.argv[1])," is", val)
+    else:
+        print("Error in pin no")
+
+
+    val = api_gpio.api_pa_pc_write_gpio(int(sys.argv[2]), int(sys.argv[3]))
+    if val != g_GPIO_ERROR:
+        print("Written successfully")
+    else:
+        print("error in pin no")
+
+
+    val = api_gpio.api_pa_pc_read_gpio(int(sys.argv[1]))
+    if val != g_GPIO_ERROR:
+        print(" Final Gpio vaue of  ", int(sys.argv[1]), " is = ", val)
+    else:
+        print("Error in pin no")
