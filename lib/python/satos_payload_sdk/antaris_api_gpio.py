61b1f0900cb5e5675762ab7b31a18e98d4cc07da
Rahul Bhivare <rahul.bhivare@antaris.global>
2023-07-12 12:09:54 +0530

    Updated gitignore

--- lib/python/satos_payload_sdk/antaris_api_gpio.py
+++ lib/python/satos_payload_sdk/antaris_api_gpio.py
@@ -165,26 +165,3 @@ def api_pa_pc_write_gpio(pin, value):
     op = (Device.port >> pin) & g_MASK_BIT_0
     Device.close()
     return op
-
-
-if __name__ == "__main__":
-    #sample test program
-    val = api_gpio. api_pa_pc_read_gpio(int(sys.argv[1]))
-    if val != g_GPIO_ERROR:
-        print("initial Gpio vaue of  ", int(sys.argv[1])," is", val)
-    else:
-        print("Error in pin no")
-
-
-    val = api_gpio.api_pa_pc_write_gpio(int(sys.argv[2]), int(sys.argv[3]))
-    if val != g_GPIO_ERROR:
-        print("Written successfully")
-    else:
-        print("error in pin no")
-
-
-    val = api_gpio.api_pa_pc_read_gpio(int(sys.argv[1]))
-    if val != g_GPIO_ERROR:
-        print(" Final Gpio vaue of  ", int(sys.argv[1]), " is = ", val)
-    else:
-        print("Error in pin no")
