79c70a469640bccc49070859d0faf852f5e86fad
Rahul Bhivare <rahul.bhivare@antaris.global>
2023-10-23 19:04:16 +0530

    Python compilation

--- tools/package-python-lib.sh
+++ tools/package-python-lib.sh
@@ -17,6 +17,11 @@
 BUILD_ROOT=`pwd`
 mkdir -p $BUILD_ROOT/dist
 
+python3 -m pip install --upgrade pip
+
 cd lib/python
+apt install -y python3.10-venv
+python3 -m pip install build
+
 python3 -m build
 mv dist/*.whl $BUILD_ROOT/dist
