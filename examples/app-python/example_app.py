b8b25be47f404559a3d82fe7b77d5ca3fee26511
shiv <shiv@geminustech.com>
2024-08-22 07:16:52 +0000

    commited the changes of app.py and antaris_api_can.py to enable can communication

--- examples/app-python/example_app.py
+++ examples/app-python/example_app.py
@@ -20,6 +20,7 @@ import os
 import sys
 import time
 import serial
+import threading
 
 from satos_payload_sdk import app_framework
 from satos_payload_sdk import antaris_api_gpio as api_gpio
@@ -165,7 +166,7 @@ class Controller:
 
         # Define the CAN channel to use (assuming the first device)
         channel = canInfo.can_dev[0]
-        logger.info("Starting CAN receiver port", channel)
+        logger.info("Starting CAN receiver port %s", channel)
 
         # Starting CAN received thread
         api_can.api_pa_pc_start_can_receiver_thread(channel)
@@ -183,7 +184,7 @@ class Controller:
             api_can.api_pa_pc_send_can_message(channel, arb_id, data_bytes)
             time.sleep(1)
 
-        logger.info("Data send = ", api_can.api_pa_pc_get_can_message_received_count())
+        logger.info("Data send = %d", api_can.api_pa_pc_get_can_message_received_count())
 
         while api_can.api_pa_pc_get_can_message_received_count() > 0: 
             received_data = api_can.api_pa_pc_read_can_data()
