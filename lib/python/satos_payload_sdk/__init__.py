af1c08ca07b932acf25b0d1932019324acfcffd2
Santosh Kavhar <santosh.kavhar@geminustech.com>
2024-08-27 11:18:12 +0530

    comment removed related to debug.

--- lib/python/satos_payload_sdk/__init__.py
+++ lib/python/satos_payload_sdk/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 if __name__ == '__main__':
-    # DEBUG = os.environ.get('DEBUG')
     logging.basicConfig(    format="%(asctime)s  %(levelname)s %(message)s",
                             datefmt="%Y-%m-%d %H:%M:%S"
                         )
