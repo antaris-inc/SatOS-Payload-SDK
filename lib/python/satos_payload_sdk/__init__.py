b8300548404496932f9b47ae588feb235efc147a
Santosh Kavhar <santosh.kavhar@geminustech.com>
2024-08-30 15:04:38 +0530

    logging removed in __init__ file as it won't be called separaretly.

--- lib/python/satos_payload_sdk/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-if __name__ == '__main__':
-    logging.basicConfig(    format="%(asctime)s  %(levelname)s %(message)s",
-                            datefmt="%Y-%m-%d %H:%M:%S",
-                            level=logging.DEBUG
-                        )
