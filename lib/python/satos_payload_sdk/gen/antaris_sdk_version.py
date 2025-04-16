793d02febc97f7c1b717d16bc34df82ebab3f9d7
rahulbhivare <44466517+rahulbhivare@users.noreply.github.com>
2025-04-16 13:08:25 +0530

    Pipeline fix (#75)
    * pipeline fix for python
    
    * Added package for cpp

--- lib/python/satos_payload_sdk/gen/antaris_sdk_version.py
+++ lib/python/satos_payload_sdk/gen/antaris_sdk_version.py
@@ -11,7 +11,7 @@ This is an auto-generated file. Any manual changes to this file will neither be 
 ANTARIS_SDK_BUILD_USER="uid=0(root) gid=0(root) groups=0(root)"
 
 
-ANTARIS_SDK_BUILD_TIME="Tue Apr  8 06:01:00 UTC 2025"
+ANTARIS_SDK_BUILD_TIME="Wed Apr 16 07:01:43 UTC 2025"
 
 
 ANTARIS_PA_PC_SDK_VERSION='1.1.1'
