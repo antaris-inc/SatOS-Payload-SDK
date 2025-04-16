b4f44fe990373008fc55dc37a0f933c50b9fb4fe
Rahul Bhivare <rahul.bhivare@antaris.global>
2025-04-16 12:48:34 +0530

    Added package for cpp

--- lib/cpp/gen/antaris_sdk_version.h
+++ lib/cpp/gen/antaris_sdk_version.h
@@ -15,7 +15,7 @@
 #define ANTARIS_SDK_BUILD_USER       "uid=0(root) gid=0(root) groups=0(root)"
 
 
-#define ANTARIS_SDK_BUILD_TIME       "Tue Apr  8 06:01:00 UTC 2025"
+#define ANTARIS_SDK_BUILD_TIME       "Wed Apr 16 07:01:43 UTC 2025"
 
 
 #define ANTARIS_PA_PC_SDK_MAJOR_VERSION             1
