747aeb909f6702ec4df696ef34e71ece7727a7bc
Rahul Bhivare <rahul.bhivare@antaris.global>
2025-06-27 13:29:10 +0530

    Updated lib

--- lib/cpp/gen/antaris_sdk_version.h
+++ lib/cpp/gen/antaris_sdk_version.h
@@ -15,7 +15,7 @@
 #define ANTARIS_SDK_BUILD_USER       "uid=0(root) gid=0(root) groups=0(root)"
 
 
-#define ANTARIS_SDK_BUILD_TIME       "Fri Jun 27 07:00:02 UTC 2025"
+#define ANTARIS_SDK_BUILD_TIME       "Fri Jun 27 07:55:25 UTC 2025"
 
 
 #define ANTARIS_PA_PC_SDK_MAJOR_VERSION             1
