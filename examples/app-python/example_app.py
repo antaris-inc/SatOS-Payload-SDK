723fdf4ff5668343f3b27233e3ae34886f201607
Shiv Singh <shiv.singh@antaris.global>
2025-08-01 11:45:09 +0000

    bug fix

--- examples/app-python/example_app.py
+++ examples/app-python/example_app.py
@@ -207,7 +207,6 @@ class Controller:
         logger.info("Handling payload power")
         power_state = ctx.params      # 0 = power off, 1 = power on
         hw_id = 0x4001                # If hw_id = 0, then default payload hardware id is send
-        power_state = 1
         if(power_state != 0 and power_state != 1):
             logger.info("invlaid power state. power state can only be 0 or 1")
             return
