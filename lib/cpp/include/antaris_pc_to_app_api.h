fd68397d430e295c95490620accaaa8410a49d5c
Shiv Singh <shiv.singh@antaris.global>
2025-11-26 13:07:54 +0000

    File Transfer Implementation between Payload server and edge server

--- lib/cpp/include/antaris_pc_to_app_api.h
+++ lib/cpp/include/antaris_pc_to_app_api.h
@@ -45,6 +45,7 @@ typedef enum {
     e_app2PC_StopSesThermMgmntReq,
     e_app2PC_SesTempReq,
     e_app2PC_PaSatOsMsg,
+    e_app2PC_PstoEsFtmOperation,
     e_app2PC_MaxRequest
 } AppToPCCallbackId_e;
 
@@ -74,6 +75,7 @@ typedef union {
     SesTempReq                      ses_temp_req;
     PaSatOsMsg                      pa_satos_msg;
     AntarisAppSdkVersion_t          sdk_version;
+    PstoEsFtmOperation              pstoes_ftm_operation;
 } AppToPCCallbackParams_t;
 
 typedef    UINT16 SHORT_APP_ID_t;
@@ -111,6 +113,7 @@ typedef enum {
     e_PC2App_SesThrmlNtf,
     e_PC2App_respPaSatOsMsg,
     e_PC2App_NtfRemoteAcPowerStatus,
+    e_PC2App_PstoEsFtmOperationNotify,
     e_PC2App_MaxRequest,
 } PCToAppApiId_e;
 
@@ -135,6 +138,7 @@ typedef union {
     SesThermalStatusNtf                 ses_thermal_ntf;
     RespPaSatOsMsg                      pa_satos_msg_response;
     NtfRemoteAcPwrStatus                remote_app_status;
+    PstoEsFtmOperationNotify            pstoes_ftm_operation_notify;
 } PCToAppApiParams_t;
 
 PCToAppClientContext an_pc_pa_create_client(INT8 *peer_ip_str, UINT16 port, INT8 *client_ssl_addr, UINT32 ssl_flag);
