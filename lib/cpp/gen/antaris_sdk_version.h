9952ca0ddc63608b5b9f4c461ea32e9ff34c7150
Rahul Bhivare <rahul.bhivare@antaris.global>
2025-11-27 10:30:33 +0000

    Updated grpc file for python

--- lib/cpp/gen/antaris_sdk_version.h
+++ lib/cpp/gen/antaris_sdk_version.h
@@ -15,7 +15,7 @@
 #define ANTARIS_SDK_BUILD_USER       "uid=0(root) gid=0(root) groups=0(root)"
 
 
-#define ANTARIS_SDK_BUILD_TIME       "Fri Oct 10 08:53:09 UTC 2025"
+#define ANTARIS_SDK_BUILD_TIME       "Thu Nov 27 10:22:30 UTC 2025"
 
 
 #define ANTARIS_PA_PC_SDK_MAJOR_VERSION             1
