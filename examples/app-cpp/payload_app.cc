d85dcdc1a3e098d016f6d0e42e08de294a786243
shiv3993 <shiv@geminustech.com>
2025-12-23 09:05:26 +0000

    User configurable FSM sleep time  (#158)
    FSM sleep time is made user configurable, so that PA --> PC messages of higher frequency and small delay (like fetching ADCS data with periodicity of 500 ms) can be accomodated
    
    
    Co-authored-by: Shiv Singh <shiv.singh@antaris.global>

--- examples/app-cpp/payload_app.cc
+++ examples/app-cpp/payload_app.cc
@@ -31,6 +31,7 @@
 #define MAX_STR_LEN 256
 #define SEQ_PARAMS_LEN 64
 #define SEQ_NAME_LEN   32
+#define FSM_SLEEP_TIME 100*1000 //this sleep time(ms) should be configured considering the PA <--> PC communication frequency
 
 #define HelloWorld_ID                   "HelloWorld"
 #define HelloWorld_IDX                  0
@@ -981,7 +982,7 @@ void wakeup_seq_fsm(mythreadState_t *threadState)
         printf("thread state is NULL!!\n");
         return;
     }
-    sleep(1); // avoid lost wakeups by giving fsm thread time to block on its pthread_cond_wait
+    usleep(FSM_SLEEP_TIME); // avoid lost wakeups by giving fsm thread time to block on its pthread_cond_wait
     printf("called wakeup_seq_fsm for %s\n", threadState->seq_id);
     pthread_cond_signal(&threadState->condition);
 }
